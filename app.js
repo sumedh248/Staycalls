df1375ba43dcf9c8f0138f824e1685b6cd237227
sumedh248 <mainacc440@gmail.com>
2026-03-01 19:34:26 +0530

    client side validations

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
+const wrapasync = require("./utils/wrapasync.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsmate);   
-app.use(express.static(path.join(__dirname,"/public")));
+app.engine('ejs', ejsmate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 const MONGOURL = "mongodb://127.0.0.1:27017/roadguests";
@@ -29,41 +30,42 @@ async function main() {
 app.get("/", (req, res) => {
    res.send("app started");
 });
-app.get("/listing", async (req,res)=>{
+app.get("/listing", async (req, res) => {
    const allListings = await Listing.find({});
-   
-   res.render("listing/index.ejs",{allListings});
+   res.render("listing/index.ejs", { allListings });
 });
-app.get("/listing/new", (req,res)=>{
+app.get("/listing/new", (req, res) => {
    res.render("listing/new.ejs");
 });
-app.get("/listing/:id", async (req,res)=>{
-   const {id} = req.params;
+app.get("/listing/:id", async (req, res) => {
+   const { id } = req.params;
    const listingdata = await Listing.findById(id);
-   res.render("listing/show.ejs",{listingdata});
+   res.render("listing/show.ejs", { listingdata });
 });
-app.post("/listing", async (req,res)=>{
+app.post("/listing", wrapasync(async (req, res, next) => {
    const ins = new Listing(req.body.listing);
    await ins.save();
    res.redirect("/listing");
-});
-app.get("/listing/:id/edit", async (req,res)=>{
-   const {id}=req.params;
+}));
+app.get("/listing/:id/edit", async (req, res) => {
+   const { id } = req.params;
    const listing1 = await Listing.findById(id);
-   res.render("listing/edit.ejs",{listing1});
+   res.render("listing/edit.ejs", { listing1 });
 });
-app.put("/listing/:id",async (req,res)=>{
-   const {id} = req.params;
-   await Listing.findByIdAndUpdate(id , {...req.body.listing});
+app.put("/listing/:id", async (req, res) => {
+   const { id } = req.params;
+   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
    res.redirect(`/listing/${id}`);
 });
-app.get("/listing/:id/delete", async (req,res)=>{
-   const {id} = req.params;
+app.get("/listing/:id/delete", async (req, res) => {
+   const { id } = req.params;
    await Listing.findByIdAndDelete(id);
    res.redirect(`/listing`);
 });
 
-
+app.use((err, req, res, next)=>{
+   res.send("something wrong happened");
+})
 
 app.listen(8080, () => {
    console.log("server listening on port 8080");
